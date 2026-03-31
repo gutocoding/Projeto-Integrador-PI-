24c8da30798bf14dd9b4c45ebf727058888f4fec
Augusto <gutocoding@gmail.com>
2026-03-31 12:18:29 -0300

    Adicionado medicao  de  tempo

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "src/produtos.h"
 #include "src/busca.h"
+#include "src/medirtempo.h"
 
 int main(void)
 {
+    //Lendo produtos da planilha e armazenando em um vetor
     int total_reg = 0;
     Produto *produtos = ler_produtos(&total_reg);
 
+    //Testando funcao buscar
     int bbusca = buscar(produtos, total_reg, 54165);
     if (bbusca != -1)
     {
@@ -30,7 +34,17 @@ int main(void)
         printf("ID 900000000 nao encontrado.\n");
     }
 
+    //medindo o tempo
 
+    printf(">>> Medindo pior caso (ultimo ID da lista)...\n");
+    medir_tempo(produtos, total_reg, 189553);
+
+    printf(">>> Medindo o caso mediano (ID no meio da lista)...\n");
+    medir_tempo(produtos, total_reg, 127776);
+
+    printf(">>> Medindo o melhor caso (Primeiro ID da lista)...\n");
+    medir_tempo(produtos, total_reg, 43614);
+    
     return 0;
     
 }
